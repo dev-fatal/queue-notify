c20534786e502d9723e3b4b91e9897dd9ce83ce8
dev-fatal <fatal.conviction@proton.me>
2023-02-11 17:04:31 +0000

    Now only dumping to config if we have made a change to it

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ def load_config():
 
     if not config["token"]:  # No token supplied
         sys.exit("Please enter a token in the config file")
+
+    updated_config = False
     while not config["chat_id"]:  # Keep trying to find chat id until we get a valid one
+        updated_config = True
         input("Please send a message to the Telegram bot you created. Once done, wait 1 \
             minute then press Enter. If it doesn't work, send another message then try \
             again.")
         config["chat_id"] = get_chat_id(config["token"])
 
-    with open(config_location, "w") as file:
-        toml.dump(config, file)  # Output new config (to file) with updated chat id
+    if updated_config:
+        with open(config_location, "w") as file:
+            toml.dump(config, file)  # Output new config (to file) with updated chat id
+
     return config
 
 
